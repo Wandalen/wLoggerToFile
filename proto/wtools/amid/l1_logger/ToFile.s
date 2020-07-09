8b0ee6cca6cae34ee6666ca00e489291e3cf2499
wandalen <wandalen.me@gmail.com>
2020-07-09 09:26:19 +0300

    .

--- proto/wtools/amid/l1_logger/ToFile.s
+++ proto/wtools/amid/l1_logger/ToFile.s
@@ -12,7 +12,7 @@
 if( typeof module !== 'undefined' )
 {
 
-  let _ = require( '../../../../../dwtools/Tools.s' );
+  let _ = require( '../../../../../wtools/Tools.s' );
 
   _.include( 'wLogger' );
   _.include( 'wFiles' );
